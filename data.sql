a1f8966fcfc039a370636cee13aff1279972237d
Kitsunetic <kitsune.guru@gmail.com>
2020-11-21 14:57:21 +0900

    Update data.sql

--- data.sql
+++ data.sql
@@ -22,10 +22,11 @@ CREATE TABLE `user` (
   `_id` int(10) unsigned NOT NULL AUTO_INCREMENT,
   `name` varchar(24) NOT NULL,
   `password` varchar(64) NOT NULL,
-  PRIMARY KEY (`_id`)
+  PRIMARY KEY (`_id`),
+  UNIQUE KEY `name` (`name`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 INSERT INTO `user` (`_id`, `name`, `password`) VALUES
-(1,	'admin',	'03ac674216f3e15c761ee1a5e255f067953623c8b388b4459e13f978d7c846f4');
+(1,	'admin',	'be9ad412f783dc9dd6c557e9a80cfdb4c45f0e5f4e6183e999f50026a1250c8c');
 
--- 2020-11-19 09:09:21
+-- 2020-11-21 05:57:00
